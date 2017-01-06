1cf101418ea81fcf5beab757d794487fce91b8e1
nakamura001 <polluxtk@gmail.com>
2017-01-06 14:42:06 +0900

    Initial commit.

---
+++ Assets/Editor/CreateScriptableObjectSample.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateScriptableObjectSample : MonoBehaviour {
+    const int ParamCount = 100000;
+    [MenuItem("ScriptableObject/Create")]
+    public static void CreateAsset()
+    {
+        CreateSoClass();
+        CreateSoStruct();
+    }
+
+    public static void CreateSoClass() {
+        ScriptableObjectClassSample so = ScriptableObject.CreateInstance<ScriptableObjectClassSample>();
+        so.list = new List<ScriptableObjectClassSample.Param>();
+        for (int i=0; i<ParamCount; i++) {
+            ScriptableObjectClassSample.Param param = new ScriptableObjectClassSample.Param();
+            param.Hp = Random.Range(10, 1000);
+            param.Mp = Random.Range(10, 1000);
+            so.list.Add(param);
+        }
+
+        string path = "Assets/Resources/ClassSample.asset";
+
+        AssetDatabase.DeleteAsset (path);
+        AssetDatabase.CreateAsset(so, path);
+        AssetDatabase.SaveAssets();
+    }
+
+    public static void CreateSoStruct() {
+        ScriptableObjectStructSample so = ScriptableObject.CreateInstance<ScriptableObjectStructSample>();
+        so.list = new List<ScriptableObjectStructSample.Param>();
+        for (int i=0; i<ParamCount; i++) {
+            ScriptableObjectStructSample.Param param = new ScriptableObjectStructSample.Param();
+            param.Hp = Random.Range(10, 1000);
+            param.Mp = Random.Range(10, 1000);
+            so.list.Add(param);
+        }
+
+        string path = "Assets/Resources/StructSample.asset";
+
+        AssetDatabase.DeleteAsset (path);
+        AssetDatabase.CreateAsset(so, path);
+        AssetDatabase.SaveAssets();
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+    // ScriptableObject は初回の Resources.Load で動作するが2回目以降はほぼ0秒で処理が終わる様なので
+    // LoadCount と TestCount は 1 に設定
+    const int LoadCount = 1;
+    const int TestCount = 1;
+    public InputField outputIField;
+    ScriptableObjectClassSample[] classData = new ScriptableObjectClassSample[TestCount];
+    ScriptableObjectStructSample[] structData = new ScriptableObjectStructSample[TestCount];
+
+    public void Execute() {
+        float startTime;
+        string output = "";
+        // Class
+        output += "[Class]\n";
+        for (int i=0; i<LoadCount; i++) {
+            for (int j=0; j<TestCount; j++) {
+                startTime = Time.realtimeSinceStartup;
+                classData[0] = Resources.Load<ScriptableObjectClassSample>("ClassSample");
+                output += string.Format("{0:f3}\n", Time.realtimeSinceStartup - startTime);
+            }
+        }
+
+        output += "\n";
+        // Struct
+        output += "[Struct]\n";
+        for (int i=0; i<LoadCount; i++) {
+            for (int j=0; j<TestCount; j++) {
+                startTime = Time.realtimeSinceStartup;
+                structData[j] = Resources.Load<ScriptableObjectStructSample>("StructSample");
+                output += string.Format("{0:f3}\n", Time.realtimeSinceStartup - startTime);
+            }
+        }
+
+        outputIField.text += output;
+    }
+}
